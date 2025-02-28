4b7bdaf14833af3db0ee30a0d06959e95dbc2d0c
hendrowunga <hendrowunga073@gmail.com>
2025-02-28 20:01:40 +0700

    🌍 Opportunistic Network Environment (ONE) Simulator

--- one.sh
+++ one.sh
@@ -1,2 +1,3 @@
 #! /bin/sh
-java -Xmx512M -cp bin:lib/ECLA.jar:lib/DTNConsoleConnection.jar core.DTNSim $*
+java -Xmx512M -cp "/media/endos/storage1/IdeaProjects/Pj Dillon/the-one-pitt/lib/*:/media/endos/storage1/IdeaProjects/Pj Dillon/the-one-pitt/bin" core.DTNSim "$@"
+
--- run.sh
+++ run.sh
@@ -5,4 +5,7 @@ echo "Running compile.sh..."
 ./compile.sh
 
 echo "Running one.sh..."
-./one.sh -b 1 properties/AlternateDropReport_settings.txt
+./one.sh -b 1 config/AlternateDropReport_settings.txt
+./one.sh -b 1 config/Epidemic/
+./one.sh -b 1 config/SprayAndWait/
+./one.sh -b 1 config/Prophet/
