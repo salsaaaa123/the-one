03244cc8b5bb922768455dac06e6f634537796c9
hendrowunga <hendrowunga073@gmail.com>
2025-03-21 22:20:56 +0700

    🌍 Opportunistic Network Environment (ONE) Simulator

--- src/core/Tuple.java
+++ src/core/Tuple.java
@@ -1,28 +1,26 @@
-/* 
- * Copyright 2010 Aalto University, ComNet
- * Released under GPLv3. See LICENSE.txt for details. 
- */
 package core;
 
 /**
  * A generic key-value tuple.
  */
-public class Tuple<K,V>  {
+public class Tuple<K, V> {
 	private K key;
 	private V value;
-	
-	/** 
+
+	/**
 	 * Creates a new tuple.
-	 * @param key The key of the tuple
+	 *
+	 * @param key   The key of the tuple
 	 * @param value The value of the tuple
 	 */
 	public Tuple(K key, V value) {
 		this.key = key;
 		this.value = value;
 	}
-	
+
 	/**
 	 * Returns the key
+	 *
 	 * @return the key
 	 */
 	public K getKey() {
@@ -31,17 +29,31 @@ public class Tuple<K,V>  {
 
 	/**
 	 * Returns the value
+	 *
 	 * @return the value
 	 */
 	public V getValue() {
 		return value;
 	}
-	
+
 	/**
 	 * Returns a string representation of the tuple
+	 *
 	 * @return a string representation of the tuple
 	 */
 	public String toString() {
 		return key.toString() + ":" + value.toString();
+	}
+	public K getFirst() {
+		return key;
+	}
+
+	/**
+	 * Returns the value
+	 *
+	 * @return the value
+	 */
+	public V getSecond() {
+		return value;
 	}
 }
