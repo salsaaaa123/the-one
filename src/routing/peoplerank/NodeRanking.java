2acc3bae2258d029ed4f84b3ab6360cfcafcaed5
hendrowunga <hendrowunga073@gmail.com>
2025-04-04 07:21:31 +0700

    🌍 Opportunistic Network Environment (ONE) Simulator

---
+++ src/routing/peoplerank/NodeRanking.java
@@ -0,0 +1,20 @@
+/*
+ * © 2025 Hendro Wunga, Sanata Dharma University, Network Laboratory
+ */
+
+package routing.peoplerank;
+
+public interface NodeRanking {
+    /**
+     * Mengembalikan nilai PeopleRank dari node saat ini.
+     * @return nilai PeopleRank
+     */
+    double getPeopleRank();
+
+    /**
+     * Membandingkan peringkat antara dua node.
+     * @param otherNode node lain yang akan dibandingkan
+     * @return true jika node saat ini memiliki peringkat lebih tinggi dari otherNode
+     */
+    boolean hasHigherRankThan(NodeRanking otherNode);
+}
