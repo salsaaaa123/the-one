b34fad19b7f3834de6a7c70cd1665d7d346513c2
hendrowunga <hendrowunga073@gmail.com>
2025-05-20 01:53:40 +0700

    feat; Update modification ProphetRouter

--- src/routing/ProphetRouter.java
+++ src/routing/ProphetRouter.java
@@ -20,25 +20,26 @@ import core.SimClock;
 import core.Tuple;
 
 /**
- * Implementation of PRoPHET router as described in 
+ * Implementation of PRoPHET router as described in
  * <I>Probabilistic routing in intermittently connected networks</I> by
  * Anders Lindgren et al.
  */
 public class ProphetRouter extends ActiveRouter {
-	/** delivery predictability initialization constant*/
+	/** delivery predictability initialization constant */
 	public static final double P_INIT = 0.75;
 	/** delivery predictability transitivity scaling constant default value */
 	public static final double DEFAULT_BETA = 0.25;
 	/** delivery predictability aging constant */
 	public static final double GAMMA = 0.98;
 
-	/** Prophet router's setting namespace ({@value})*/
+	/** Prophet router's setting namespace ({@value}) */
 	public static final String PROPHET_NS = "ProphetRouter";
 	/**
 	 * Number of seconds in time unit -setting id ({@value}).
-	 * How many seconds one time unit is when calculating aging of 
-	 * delivery predictions. Should be tweaked for the scenario.*/
-	public static final String SECONDS_IN_UNIT_S ="secondsInTimeUnit";
+	 * How many seconds one time unit is when calculating aging of
+	 * delivery predictions. Should be tweaked for the scenario.
+	 */
+	public static final String SECONDS_IN_UNIT_S = "secondsInTimeUnit";
 
 	/**
 	 * Transitivity scaling constant (beta) -setting id ({@value}).
@@ -59,6 +60,7 @@ public class ProphetRouter extends ActiveRouter {
 	/**
 	 * Constructor. Creates a new message router based on the settings in
 	 * the given Settings object.
+	 * 
 	 * @param s The settings object
 	 */
 	public ProphetRouter(Settings s) {
@@ -67,8 +69,7 @@ public class ProphetRouter extends ActiveRouter {
 		secondsInTimeUnit = prophetSettings.getInt(SECONDS_IN_UNIT_S);
 		if (prophetSettings.contains(BETA_S)) {
 			beta = prophetSettings.getDouble(BETA_S);
-		}
-		else {
+		} else {
 			beta = DEFAULT_BETA;
 		}
 
@@ -77,6 +78,7 @@ public class ProphetRouter extends ActiveRouter {
 
 	/**
 	 * Copyconstructor.
+	 * 
 	 * @param r The router prototype where setting values are copied from
 	 */
 	protected ProphetRouter(ProphetRouter r) {
@@ -105,6 +107,7 @@ public class ProphetRouter extends ActiveRouter {
 	/**
 	 * Updates delivery predictions for a host.
 	 * <CODE>P(a,b) = P(a,b)_old + (1 - P(a,b)_old) * P_INIT</CODE>
+	 * 
 	 * @param host The host we just met
 	 */
 	private void updateDeliveryPredFor(DTNHost host) {
@@ -116,6 +119,7 @@ public class ProphetRouter extends ActiveRouter {
 	/**
 	 * Returns the current prediction (P) value for a host or 0 if entry for
 	 * the host doesn't exist.
+	 * 
 	 * @param host The host to look the P for
 	 * @return the current P value
 	 */
@@ -123,8 +127,7 @@ public class ProphetRouter extends ActiveRouter {
 		ageDeliveryPreds(); // make sure preds are updated before getting
 		if (preds.containsKey(host)) {
 			return preds.get(host);
-		}
-		else {
+		} else {
 			return 0;
 		}
 	}
@@ -133,6 +136,7 @@ public class ProphetRouter extends ActiveRouter {
 	 * Updates transitive (A->B->C) delivery predictions.
 	 * <CODE>P(a,c) = P(a,c)_old + (1 - P(a,c)_old) * P(a,b) * P(b,c) * BETA
 	 * </CODE>
+	 * 
 	 * @param host The B host who we just met
 	 */
 	private void updateTransitivePreds(DTNHost host) {
@@ -141,8 +145,7 @@ public class ProphetRouter extends ActiveRouter {
 				" with other routers of same type";
 
 		double pForHost = getPredFor(host); // P(a,b)
-		Map<DTNHost, Double> othersPreds =
-				((ProphetRouter)otherRouter).getDeliveryPreds();
+		Map<DTNHost, Double> othersPreds = ((ProphetRouter) otherRouter).getDeliveryPreds();
 
 		for (Map.Entry<DTNHost, Double> e : othersPreds.entrySet()) {
 			if (e.getKey() == getHost()) {
@@ -150,7 +153,7 @@ public class ProphetRouter extends ActiveRouter {
 			}
 
 			double pOld = getPredFor(e.getKey()); // P(a,c)_old
-			double pNew = pOld + ( 1 - pOld) * pForHost * e.getValue() * beta;
+			double pNew = pOld + (1 - pOld) * pForHost * e.getValue() * beta;
 			preds.put(e.getKey(), pNew);
 		}
 	}
@@ -159,6 +162,7 @@ public class ProphetRouter extends ActiveRouter {
 	 * Ages all entries in the delivery predictions.
 	 * <CODE>P(a,b) = P(a,b)_old * (GAMMA ^ k)</CODE>, where k is number of
 	 * time units that have elapsed since the last time the metric was aged.
+	 * 
 	 * @see #SECONDS_IN_UNIT_S
 	 */
 	private void ageDeliveryPreds() {
@@ -171,7 +175,7 @@ public class ProphetRouter extends ActiveRouter {
 
 		double mult = Math.pow(GAMMA, timeDiff);
 		for (Map.Entry<DTNHost, Double> e : preds.entrySet()) {
-			e.setValue(e.getValue()*mult);
+			e.setValue(e.getValue() * mult);
 		}
 
 		this.lastAgeUpdate = SimClock.getTime();
@@ -179,6 +183,7 @@ public class ProphetRouter extends ActiveRouter {
 
 	/**
 	 * Returns a map of this router's delivery predictions
+	 * 
 	 * @return a map of this router's delivery predictions
 	 */
 	private Map<DTNHost, Double> getDeliveryPreds() {
@@ -189,8 +194,8 @@ public class ProphetRouter extends ActiveRouter {
 	@Override
 	public void update() {
 		super.update();
-		if (!canStartTransfer() ||isTransferring()) {
-			return; // nothing to transfer or is currently transferring 
+		if (!canStartTransfer() || isTransferring()) {
+			return; // nothing to transfer or is currently transferring
 		}
 
 		// try messages that could be delivered to final recipient
@@ -204,20 +209,22 @@ public class ProphetRouter extends ActiveRouter {
 	/**
 	 * Tries to send all other messages to all connected hosts ordered by
 	 * their delivery probability
+	 * 
 	 * @return The return value of {@link #tryMessagesForConnected(List)}
 	 */
 	private Tuple<Message, Connection> tryOtherMessages() {
-		List<Tuple<Message, Connection>> messages =
-				new ArrayList<Tuple<Message, Connection>>();
+		List<Tuple<Message, Connection>> messages = new ArrayList<Tuple<Message, Connection>>();
 
 		Collection<Message> msgCollection = getMessageCollection();
-		
-		/* for all connected hosts collect all messages that have a higher
-		   probability of delivery by the other host */
-		for(Connection con : getHost()) {
-			//for (Connection con : getConnections()) {
+
+		/*
+		 * for all connected hosts collect all messages that have a higher
+		 * probability of delivery by the other host
+		 */
+		for (Connection con : getHost()) {
+			// for (Connection con : getConnections()) {
 			DTNHost other = con.getOtherNode(getHost());
-			ProphetRouter othRouter = (ProphetRouter)other.getRouter();
+			ProphetRouter othRouter = (ProphetRouter) other.getRouter();
 
 			if (othRouter.isTransferring()) {
 				continue; // skip hosts that are transferring
@@ -229,7 +236,7 @@ public class ProphetRouter extends ActiveRouter {
 				}
 				if (othRouter.getPredFor(m.getTo()) > getPredFor(m.getTo())) {
 					// the other node has higher probability of delivery
-					messages.add(new Tuple<Message, Connection>(m,con));
+					messages.add(new Tuple<Message, Connection>(m, con));
 				}
 			}
 		}
@@ -240,37 +247,44 @@ public class ProphetRouter extends ActiveRouter {
 
 		// sort the message-connection tuples
 		Collections.sort(messages, new TupleComparator());
-		return tryMessagesForConnected(messages);	// try to send messages
+		return tryMessagesForConnected(messages); // try to send messages
 	}
 
 	/**
 	 * Comparator for Message-Connection-Tuples that orders the tuples by
-	 * their delivery probability by the host on the other side of the 
+	 * their delivery probability by the host on the other side of the
 	 * connection (GRTRMax)
 	 */
-	private class TupleComparator implements Comparator
-			<Tuple<Message, Connection>> {
+	private class TupleComparator implements Comparator<Tuple<Message, Connection>> {
 
 		public int compare(Tuple<Message, Connection> tuple1,
-						   Tuple<Message, Connection> tuple2) {
+				Tuple<Message, Connection> tuple2) {
 			// delivery probability of tuple1's message with tuple1's connection
-			double p1 = ((ProphetRouter)tuple1.getValue().
-					getOtherNode(getHost()).getRouter()).getPredFor(
+			double p1 = ((ProphetRouter) tuple1.getValue().getOtherNode(getHost()).getRouter()).getPredFor(
 					tuple1.getKey().getTo());
 			// -"- tuple2...
-			double p2 = ((ProphetRouter)tuple2.getValue().
-					getOtherNode(getHost()).getRouter()).getPredFor(
+			double p2 = ((ProphetRouter) tuple2.getValue().getOtherNode(getHost()).getRouter()).getPredFor(
 					tuple2.getKey().getTo());
 
 			// bigger probability should come first
-			if (p2-p1 == 0) {
+			if (p2 - p1 == 0) {
 				/* equal probabilities -> let queue mode decide */
 				return compareByQueueMode(tuple1.getKey(), tuple2.getKey());
-			}
-			else if (p2-p1 < 0) {
+				// String id1 = tuple1.getKey().getId();
+				// String id2 = tuple2.getKey().getId();
+				// return id1.compareTo(id2);
+				/* equal probabilities -> break tie using message receive time (FIFO-like) */
+				// double time1 = tuple1.getKey().getReceiveTime(); // Gunakan waktu terima
+				// double time2 = tuple2.getKey().getReceiveTime(); // Gunakan waktu terima
+
+				// if (time1 < time2)
+				// return -1; // Pesan yang diterima lebih dulu (<) didahulukan (-1)
+				// if (time1 > time2)
+				// return 1;
+				// return 0; // Waktu terima sama
+			} else if (p2 - p1 < 0) {
 				return -1;
-			}
-			else {
+			} else {
 				return 1;
 			}
 		}
